40d5e6c374f1639324c039d64ffa2bb57bdde1d4
WesternGreen <westerngreen@126.com>
2018-10-14 21:42:13 +0800

    finish battle controller

---
+++ phase1/battlecontroller.cpp
@@ -0,0 +1,66 @@
+#include "battlecontroller.h"
+
+BattleController::BattleController(Pokemon &pokemon1, Pokemon &pokemon2, bool autoFight) : p1(pokemon1), p2(pokemon2)
+{
+	_auto = autoFight;
+}
+
+bool BattleController::start()
+{
+	p1.restoreAll();
+	p2.restoreAll();
+
+	timer1 = 0;
+	timer2 = 0;
+
+	while (1)
+	{
+		while (timer1 < MAX_TIMER && timer2 < MAX_TIMER)
+		{
+			timer1 += p1.speed();
+			timer2 += p2.speed();
+		}
+		if (timer1 >= MAX_TIMER && timer2 >= MAX_TIMER)
+		{
+			if (p1.speed() >= p2.speed())
+			{
+				if (p1.attack(p2))
+					break;
+				if (p2.attack(p1))
+					break;
+			}
+			else
+			{
+				if (p2.attack(p1))
+					break;
+				if (p1.attack(p2))
+					break;
+			}
+			timer1 -= MAX_TIMER;
+			timer2 -= MAX_TIMER;
+		}
+		else if (timer1 >= MAX_TIMER)
+		{
+			if (p1.attack(p2))
+				break;
+			timer1 -= MAX_TIMER;
+		}
+		else
+		{
+			if (p2.attack(p1))
+				break;
+			timer2 -= MAX_TIMER;
+		}
+	}
+
+	if (p1.hp())
+	{
+		//p1 won
+		return true;
+	}
+	else
+	{
+		//p2 won
+		return false;
+	}
+}
---
+++ phase1/battlecontroller.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "pokemon.h"
+
+class BattleController
+{
+private:
+	static const int MAX_TIMER = 100;
+	Pokemon &p1, p2;
+	bool _auto; //auto fight
+	int timer1;
+	int timer2;
+
+public:
+	BattleController(Pokemon &pokemon1, Pokemon &pokemon2, bool autoFight = true);
+	bool start(); //return true if pokemon1 wons
+};

ba08fd7ee344b0ebc822226cd02c2997eaa6d0cf
WesternGreen <westerngreen@126.com>
2018-11-04 22:19:20 +0800

    update tester

--- phase1/tester.cpp
+++ phase1/tester.cpp
@@ -8,23 +8,23 @@ int main()
 {
 	srand(time(NULL));
 	cout << "init pokemon races ...\n";
-	Race_1 race1;
-	Race_2 race2;
-	Race_3 race3;
-	Race_4 race4;
+	Race<0> race1;
+	Race<1> race2;
+	Race<2> race3;
+	Race<3> race4;
 
-	Pokemon pokemon1(race1);
-	Pokemon pokemon2(race2);
+	Pokemon pokemon1(race1, "pokemon1");
+	Pokemon pokemon2(race2, "pokemon2");
 	Pokemon pokemon3(race3);
 	Pokemon pokemon4(race4);
 
 	pokemon1.gainExp(1000);
-	pokemon2.gainExp(1000);
-	pokemon3.gainExp(1000);
-	pokemon4.gainExp(1000);
+	 pokemon2.gainExp(1000);
+	// pokemon3.gainExp(100);
+	// pokemon4.gainExp(100);
 
 	//  BattleController battle(pokemon1, pokemon2);				//auto fight
-	BattleController battle(pokemon1, pokemon3, false); //manual fight
+	BattleController battle(pokemon1, pokemon2, false); //manual fight
 
 	battle.start();
 

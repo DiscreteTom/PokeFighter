4e44125f4501351b24b2b3228faccb7c5e0c148e
WesternGreen <westerngreen@126.com>
2018-11-05 16:41:01 +0800

    construct netconfig.h&cpp, define global config variables

---
+++ phase2/netconfig.cpp
@@ -0,0 +1,5 @@
+// define global network config variables here
+
+const int SERVER_PORT = 7500;
+const int SERVER_REQ_QUEUE_LENGTH = 4;// server request queue max length, usually 2-4
+const int BUF_LENGTH = 1024;// server buffer length MUST equals client buffer length
---
+++ phase2/netconfig.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// declare global network config variables here
+
+extern const int SERVER_PORT;
+extern const int SERVER_REQ_QUEUE_LENGTH;
+extern const int BUF_LENGTH;

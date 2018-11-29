b0beeb19d241a97319cbd005c18fae53ba12b08d
WesternGreen <westerngreen@126.com>
2018-11-29 14:51:39 +0800

    add client using QT - add start view - add login view - TODO: qss - TODO: logon window

---
+++ phase2/client/PokeFighterClient/mainwindow.cpp
@@ -0,0 +1,94 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+
+	// set style sheet
+	QFile file(":/qss/main.qss");
+	file.open(QFile::ReadOnly);
+	QTextStream filetext(&file);
+	QString stylesheet = filetext.readAll();
+	this->setStyleSheet(stylesheet);
+	file.close();
+
+	setWindowTitle(tr("宠物小精灵对战程序客户端"));
+
+	// start layout
+	lbStartTitle = new QLabel(tr("宠物小精灵对战程序"), this);
+	btnPlay = new QPushButton(tr("开始游戏"), this);
+	btnExit = new QPushButton(tr("退出"), this);
+
+
+	//login layout
+	lbLoginLabel = new QLabel(tr("登录"), this);
+	leUsername = new QLineEdit(this);
+	lePassword = new QLineEdit(this);
+	btnLogin = new QPushButton(tr("登录"), this);
+	btnLogon = new QPushButton(tr("注册"), this);
+	btnBack = new QPushButton(tr("返回"), this);
+
+	connect(btnPlay, &QPushButton::clicked, this, [this]{ changeState(LOGIN); });
+	connect(btnBack, &QPushButton::clicked, this, [this]{ changeState(START); });
+	connect(btnExit, &QPushButton::clicked, this, [this]{ qApp->quit(); });
+
+	changeState(START);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+}
+
+void MainWindow::changeState(MainWindow::State aim)
+{
+	// hide all widget
+	lbStartTitle->hide();
+	btnPlay->hide();
+	btnExit->hide();
+	lbLoginLabel->hide();
+	leUsername->hide();
+	lePassword->hide();
+	btnLogin->hide();
+	btnLogon->hide();
+	btnBack->hide();
+
+	state = aim;
+	switch(state){
+	case START:
+		delete ui->centralWidget->layout();
+		layout = new QGridLayout(this);
+		lbStartTitle->show();
+		btnPlay->show();
+		btnExit->show();
+		layout->addWidget(lbStartTitle, 0, 0, Qt::AlignCenter);
+		layout->addWidget(btnPlay, 1, 0, Qt::AlignCenter);
+		layout->addWidget(btnExit, 2, 0, Qt::AlignCenter);
+		ui->centralWidget->setLayout(layout);
+		break;
+	case LOGIN:
+		delete ui->centralWidget->layout();
+		layout = new QGridLayout(this);
+		lbLoginLabel->show();
+		leUsername->show();
+		lePassword->show();
+		btnLogin->show();
+		btnLogon->show();
+		btnBack->show();
+		layout->addWidget(lbLoginLabel, 0, 0, Qt::AlignCenter);
+		layout->addWidget(leUsername, 1, 0, Qt::AlignCenter);
+		layout->addWidget(lePassword, 2, 0, Qt::AlignCenter);
+		layout->addWidget(btnLogin, 3, 0, Qt::AlignCenter);
+		layout->addWidget(btnLogon, 4, 0, Qt::AlignCenter);
+		layout->addWidget(btnBack, 5, 0, Qt::AlignCenter);
+		ui->centralWidget->setLayout(layout);
+		break;
+	default:
+		break;
+	}
+}
---
+++ phase2/client/PokeFighterClient/mainwindow.h
@@ -0,0 +1,52 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QFrame>
+#include <QWidget>
+#include <QLabel>
+#include <QGridLayout>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+private:
+	Ui::MainWindow *ui;
+
+	enum State{
+		START,
+		LOGIN
+	};
+
+	State state;
+
+	QGridLayout * layout;
+
+	// start layout
+	QLabel * lbStartTitle;
+	QPushButton * btnPlay;
+	QPushButton * btnExit;
+
+	// login layout
+	QLabel * lbLoginLabel;
+	QLineEdit * leUsername;
+	QLineEdit * lePassword;
+	QPushButton * btnLogin;
+	QPushButton * btnLogon;
+	QPushButton * btnBack;
+
+	void changeState(State aim);
+};
+
+#endif // MAINWINDOW_H

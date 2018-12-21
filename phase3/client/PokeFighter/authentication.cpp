fe25e0b83eb24f82a718d262d16f242adfdc370d
WesternGreen <westerngreen@126.com>
2018-12-21 11:01:01 +0800

    fix authentication::isValidUsername

--- phase3/client/PokeFighter/authentication.cpp
+++ phase3/client/PokeFighter/authentication.cpp
@@ -18,7 +18,7 @@ bool isValidUsername(const QString &str)
 		return false;
 	}
 	for (auto c : str){
-		if (c == '\n' || c == '\t' || c == '\'' || c == '"' || c == '`' || c == ' ' || c == '\0'){
+		if (c == '\n' || c == '\t' || c == '\'' || c == '"' || c == '`' || c == ' '){
 			return false;
 		}
 	}
